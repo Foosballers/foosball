fc5e93f7d2a638e3055e516fd0af0169d88d82b4
Keith O'Brien <obrienkeithd@gmail.com>
2015-05-12 15:40:09 -0400

    add change listening to dashboardHeader

--- src/js/dashboardHeader.jsx
+++ src/js/dashboardHeader.jsx
@@ -3,11 +3,20 @@ var React = require('react'),
     gameStore = require('./gamestore.js');
 
 var DashboardHeader = React.createClass({
+    getInitialState: function () {
+        return {games: gameStore.getGames(), queue: gameStore.getQueue()}
+    },
+    _onChange: function(){
+        this.setState({games: gameStore.getGames(), queue: gameStore.getQueue()});
+    },
+    componentDidMount: function(){
+        gameStore.addChangeListener(this._onChange);
+    },
     render: function () {
         return <span>
                     <HeaderItem name="Current Game" game={gameStore.getCurrentGame()}/>
-                    <HeaderItem name="Last Game" game={gameStore.getGames()[0]}/>
-                    <HeaderItem name="Up Next" game={gameStore.getQueue()[0]}/>
+                    <HeaderItem name="Last Game" game={this.state.games[0]}/>
+                    <HeaderItem name="Up Next" game={this.state.queue[0]}/>
                 </span>;
     }
 });
