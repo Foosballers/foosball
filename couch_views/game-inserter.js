236bd6ede8b757fd5ec76c9567e54b8a7e67b63e
Keith O'Brien <Keith.OBrien@smartstream-stp.com>
2015-05-17 00:33:20 -0400

    add the games queued view to the inserter

--- couch_views/game-inserter.js
+++ couch_views/game-inserter.js
@@ -31,48 +31,50 @@ function insert(randomGame) {
         function (err, body, header) {
             if (err)
                 return console.error('[random_game.insert] ', err.message)
-
-            console.log('you have inserted a game.')
-            console.log(body)
         });
 }
-//
-//for (var i = 0; i < 50; i++) {
-//    var randomGame = getGame(i);
-//    insert(randomGame);
-//}
 
-//for (var i = 52; i < 58; i++) {
-//    var randomGame = getGame(i);
-//    randomGame.player1Score = null;
-//    randomGame.player2Score = null;
-//    randomGame.status = 'queued';
-//    insert(randomGame);
-//}
-foosdb.get('_design/page', function(err,existing) {
-    var page_payload_view = require('./page-payload-simplified')
-    var debug_callback= function(err, body) {
-        if(err)
-            return console.error('[views.insert] ', err)
-        console.log('views inserted')
-        console.log(body)
-    }
+for (var i = 0; i < 50; i++) {
+    var randomGame = getGame(i);
+    insert(randomGame);
+}
 
-    if(err) {
-        if(err.error !== 'not_found') {
-            console.error(err); return;
+for (var i = 52; i < 58; i++) {
+    var randomGame = getGame(i);
+    randomGame.player1Score = null;
+    randomGame.player2Score = null;
+    randomGame.status = 'queued';
+    insert(randomGame);
+}
+
+insertView('page', 'payload', 'page-payload-simplified');
+insertView('games', 'queued', 'games-queued');
+
+function insertView(designDocName, viewName, fileName){
+    var docName = '_design/' + designDocName;
+    foosdb.get(docName, function(err,existing) {
+        var page_payload_view = require('./' + fileName)
+        var debug_callback= function(err, body) {
+            if(err)
+                return console.error('[views.insert] ', err)
+            console.log('views inserted')
+            console.log(body)
         }
-        var views = {
-            views: {
-                payload: page_payload_view
+
+        if(err) {
+            if(err.error !== 'not_found') {
+                console.error(err); return;
             }
+            var views = { views: { } }
+            views.views[viewName] = page_payload_view;
+            console.log('view', views);
+            foosdb.insert(views, docName, debug_callback)
+            return
         }
-        foosdb.insert(views, '_design/page', debug_callback)
-        return
-    }
 
-    var updated_view = existing.views
-    updated_view.payload = page_payload_view
-    existing.views = updated_view
-    foosdb.insert(existing, '_design/page', debug_callback)
-})
+        var updated_view = existing.views
+        updated_view[viewName] = page_payload_view
+        existing.views = updated_view
+        foosdb.insert(existing, docName, debug_callback)
+    })
+}
--- couch_views/games-queued.js
+++ couch_views/games-queued.js
@@ -2,8 +2,12 @@
  * Created by OBrien on 5/16/2015.
  */
 
-function (doc) {
-    if (doc.status === 'queued') {
-        emit(doc._id, doc);
+var view = {
+    map: function (doc) {
+        if (doc.status === 'queued') {
+            emit(doc._id, doc);
+        }
     }
 }
+
+module.exports = view;
