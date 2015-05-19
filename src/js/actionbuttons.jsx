ac04738f7968c286eeb74d52086ffb39adc85e4a
Keith O'Brien <Keith.OBrien@smartstream-stp.com>
2015-05-18 21:59:14 -0400

    introducing a modal for game queuing. there is a bug where it only opens once. it does not do anything yet

--- src/js/actionbuttons.jsx
+++ src/js/actionbuttons.jsx
@@ -1,35 +1,68 @@
 var React = require('react'),
     constants = require('./NotificationConstants'),
     gameActions = require('./gamesActionCreator'),
-    gameStore = require('./gamestore');
+    gameStore = require('./gamestore'),
+    Modal = require('react-modal');
+
+Modal.setAppElement(document.getElementById('body'));
+Modal.injectCSS();
 
 var actionbuttons = React.createClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {};
     },
-    _onStartNextGameClick: function(){
+    _onStartNextGameClick: function () {
         var queue = gameStore.getQueue();
-        if(queue.length > 0){
+        if (queue.length > 0) {
             gameActions.startGame(queue[0]);
         }
     },
-    _onStartGameClick: function(){
+    _onStartGameClick: function () {
         console.log('start game');
     },
-    _onQueueGameClick: function(){
+    _onQueueGameClick: function () {
+        this.setState({queuingGame: true});
         console.log('queue game');
+    },
+    closeModal: function () {
+        this.setState({queuingGame: false});
     },
     render: function () {
 
         return <span>
-            <button onClick={this._onStartNextGameClick} type="button" className="btn btn-primary">Start Next Game</button>&nbsp;
-            <button onClick={this._onStartGameClick} type="button" className="btn btn-primary">Start Game</button>&nbsp;
+            <button onClick={this._onStartNextGameClick} type="button" className="btn btn-primary">Start Next Game
+            </button>
+            &nbsp;
+            <button onClick={this._onStartGameClick} type="button" className="btn btn-primary">Start Game</button>
+            &nbsp;
             <button onClick={this._onQueueGameClick} type="button" className="btn btn-primary">Queue Game</button>
+            <Modal isOpen={this.state.queuingGame} onRequestClose={this.closeModal}>
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        Add a game to the queue
+                    </div>
+                    <div className="panel-body">
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <div class="form-group">
+                                    <div className="panel-body">
+                                        <div className="form-group input-group">
+                                            <input className="form-control" placeholder="Player 1"></input>
+                                        </div>
+                                        <div className="form-group input-group">
+                                            <input className="form-control" placeholder="Player 2"></input>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+                <div>
 
-            <div>
-
-            </div>
-        </span>
+                </div>
+            </span>
     }
 });
 
