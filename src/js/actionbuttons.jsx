863c162d2eed1fb12414a219d37417e22fb1c5c1
Max P Haley <maxphaley@gmail.com>
2015-05-19 19:29:56 -0400

    added sample modal stuff using mixins

--- src/js/actionbuttons.jsx
+++ src/js/actionbuttons.jsx
@@ -2,76 +2,55 @@ var React = require('react'),
     constants = require('./NotificationConstants'),
     gameActions = require('./gamesActionCreator'),
     gameStore = require('./gamestore'),
-    Modal = require('react-modal');
+    Modalizer = require('./modal-izer.jsx')
 
-Modal.setAppElement(document.getElementById('body'));
-Modal.injectCSS();
 
-var actionbuttons = React.createClass({
-    getInitialState: function () {
-        return {};
-    },
-    _onStartNextGameClick: function () {
-        var queue = gameStore.getQueue();
-        if (queue.length > 0) {
-            gameActions.startGame(queue[0]);
-        }
-    },
-    _onStartGameClick: function () {
-        console.log('start game');
-    },
-    _onQueueGameClick: function () {
-        this.setState({queuingGame: true});
-        console.log('queue game');
-    },
-    closeModal: function () {
-        this.setState({queuingGame: false});
-    },
-    queueGame: function(){
-        console.log(this.refs.player1.getDOMNode().value);
-        console.log(this.refs.player2.getDOMNode().value);
-        gameActions.queueGame({player1: this.refs.player1.getDOMNode().value, player2: this.refs.player2.getDOMNode().value})
-    },
-    render: function () {
-
-        return <span>
-            <button onClick={this._onStartNextGameClick} type="button" className="btn btn-primary">Start Next Game
-            </button>
-            &nbsp;
-            <button onClick={this._onStartGameClick} type="button" className="btn btn-primary">Start Game</button>
-            &nbsp;
-            <button onClick={this._onQueueGameClick} type="button" className="btn btn-primary">Queue Game</button>
-            <Modal isOpen={this.state.queuingGame} onRequestClose={this.closeModal}>
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        Add a game to the queue
-                    </div>
-                    <div className="panel-body">
-                        <div className="row">
-                            <div className="col-lg-4">
-                                <div class="form-group">
-                                    <div className="panel-body">
-                                        <div className="form-group input-group">
-                                            <input className="form-control" ref="player1" placeholder="Player 1"></input>
-                                        </div>
-                                        <div className="form-group input-group">
-                                            <input className="form-control" ref="player2" placeholder="Player 2"></input>
-                                        </div>
-                                        <div className="form-group input-group">
-                                            <button type="button" className="btn btn-primary" onClick={this.queueGame}>Submit</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Modal>
-                <div>
-
-                </div>
-            </span>
+module.exports = React.createClass({
+  getInitialState: function() {
+    return { }
+  }
+ 
+, render: function() {
+    var buttons = [
+        {type: 'primary', text: 'game on', handler: this._onStartGameClick}
+      , {type: 'danger',  text: 'cancel',  handler: this.handleExternalHide}
+      ]
+    return <div className="panel panel-default">
+        <button type="button" className="btn btn-primary"
+            onClick={this.handleShowModal}>Start Next</button>&nbsp;&nbsp;
+        <button type="button" className="btn btn-primary"
+            onClick={this._onStartGameClick}>Start Game</button>&nbsp;&nbsp;
+        <button type="button" className="btn btn-primary"
+            onClick={this._onQueueGameClick}>Queue Game</button>
+        <Modalizer ref="gamemodal"
+            show={false}
+            header="Start Game"
+            buttons={buttons}>
+            <p>I said what?</p>
+        </Modalizer>
+    </div>
+  }
+ 
+, handleShowModal: function() {
+    this.refs.gamemodal.show();
+  }
+, handleExternalHide: function() {
+    this.refs.gamemodal.hide();
+  }
+, handleDoingNothing: function() {
+  }
+, _onStartNextGameClick: function () {
+    var queue = gameStore.getQueue();
+      if (queue.length > 0) {
+      gameActions.startGame(queue[0]);
     }
-});
-
-module.exports = actionbuttons;
+  }
+, _onStartGameClick: function () {
+    console.log('start game');
+    this.refs.gamemodal.hide();
+  }
+, _onQueueGameClick: function () {
+    this.setState({queuingGame: true});
+    console.log('queue game');
+  }
+})
